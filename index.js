c4c2b8d5ea58e5bceeeb5f68e20687970041ad0a
Krzysztof Szporer <krzysztof.szporer@bshg.com>
2017-03-29 13:37:48 +0200

    Added MongoDB integration.

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const http = require('http')
 
 // private
 const app = require('./lib/app');
+const database = require('./lib/database');
 
-var port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 const mainApp = app();
 const server = http.createServer(mainApp);
 
-var port = process.env.PORT || 3000
-server.listen(port, () => {
+let connectionString = null;
+
+if (process.env.VCAP_SERVICES) {
+  let env = JSON.parse(process.env.VCAP_SERVICES);
+  connectionString = env['compose-for-mongodb'][0].credentials.uri;
+} else {
+  connectionString = 'mongodb://127.0.0.1:27017/recipes';
+}
+
+database.initialize(connectionString, err => {
+  if (err) {
+    console.log('Failed to connect to database.' + err);
+    process.exit(1);
+  }
+  
+  server.listen(port, () => {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  });
 });
